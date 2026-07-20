5c4d2117615b7f92801cd0f257f3709c7df557dd
Hlaluko <ngobenih45@gmail.com>
2026-07-20 21:48:21 +0200

    Delete particle background CSS styles
    Removed particle background styles from script.js.

--- script.js
+++ script.js
@@ -243,29 +243,6 @@ if (heroTitle) {
     typing();
 
 }
-/* ==========================
-   PARTICLE BACKGROUND
-========================== */
-
-.hero{
-    position:relative;
-    overflow:hidden;
-}
-
-#particles-js{
-    position:absolute;
-    top:0;
-    left:0;
-    width:100%;
-    height:100%;
-    z-index:1;
-}
-
-.hero-content,
-.hero-image{
-    position:relative;
-    z-index:2;
-}
 
 // ===============================
 // ACTIVE NAVIGATION
