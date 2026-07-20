11d6de72f5ddbdf17191a1fc9a9e1f9301714a7f
Hlaluko <ngobenih45@gmail.com>
2026-07-20 21:22:59 +0200

    Implement counter increment feature
    Added a counter functionality that increments to a target value.

--- script.js
+++ script.js
@@ -182,6 +182,35 @@ counters.forEach(counter => {
     };
 
 });
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+const update = () => {
+
+const target = +counter.getAttribute("data-target");
+
+const count = +counter.innerText;
+
+const increment = target / 100;
+
+if(count < target){
+
+counter.innerText = Math.ceil(count + increment);
+
+setTimeout(update,20);
+
+}else{
+
+counter.innerText = target + "+";
+
+}
+
+};
+
+update();
+
+});
 
 // ===============================
 // HERO TYPING EFFECT
